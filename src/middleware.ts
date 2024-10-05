fa52c1823aceed573f001f69ac9d09270a19116e
Md Rifat <mdrifatahmed787@gmail.com>
2024-10-06 03:12:45 +0600

    profile protect by middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-import { jwtDecode } from "jwt-decode";
+
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function middleware(request: NextRequest) {
@@ -9,21 +9,8 @@ export async function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL("/", request.url));
   }
   
-  // let user;
-  // try {
-  //   user = jwtDecode(token);  
-  // } catch (error) {
-  //   return NextResponse.redirect(new URL("/admin-login", request.url));
-  // }
-  
-//   console.log("**user", user);
-//   if (user?.role === "ADMIN") {
-//     return NextResponse.next();
-//   } else {
-//     return NextResponse.redirect(new URL("/admin-login", request.url));
-//   }
 }
 
 export const config = {
-  matcher: ["/cart", "/my_order", "/return_order", "/cancel_order", "/wishlist", "/profile"],
+  matcher: ["/profile"],
 };
